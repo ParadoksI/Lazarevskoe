38d720958ba971dd973e382acdd5d1f2a47a70c9
Vendigo <119477860+Vendifo@users.noreply.github.com>
2024-02-15 09:51:21 +0900

    ГЛАВНАЯ СТРАНИЦАААААААААААААААА

--- assets/js/burger-menu.js
+++ assets/js/burger-menu.js
@@ -10,3 +10,4 @@ document.addEventListener('DOMContentLoaded', function () {
         closeIcon.classList.toggle('hide');
     });
 });
+
---
+++ assets/js/script-slider.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    $('.choice__list').slick({
+
+        slidesToScroll: 1,
+        // autoplay: true,
+        autoplaySpeed: 5000,
+
+        responsive: [
+            {
+                breakpoint: 699,
+                settings: {
+                    slidesToShow: 2,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint : 1199,
+                settings : {
+                    slidesToShow: 3,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint : 9999,
+                settings : { 
+                    slidesToShow: 4,
+                    dots: false,
+                    arrows: true,
+                }
+            }
+
+        ]
+    });
+
+    $('.eat__list').slick({
+
+        slidesToScroll: 1,
+        // autoplay: true,
+        autoplaySpeed: 5000,
+
+        responsive: [
+            {
+                breakpoint: 699,
+                settings: {
+                    slidesToShow: 2,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint : 1199,
+                settings : {
+                    slidesToShow: 3,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint : 9999,
+                settings : { 
+                    slidesToShow: 4,
+                    dots: false,
+                    arrows: true,
+                }
+            }
+
+        ]
+    });
+});
